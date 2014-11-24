b682199351ce865cd41249def2a9c31d8a2d4158
Brett Graham <brettgraham@gmail.com>
2014-11-23 19:37:36 -0500

    merging jsui

--- wsrpc/__init__.py
+++ wsrpc/__init__.py
@@ -37,18 +37,11 @@ error codes (negative):
     -32000 to -32099 Server error
 """
 
-import os
-
 from . import errors
 from . import protocol
-from . import wrapper
-
-if os.environ.get('WSRPC_USE_GEVENT', False):
-    from . import geventserver as serve
-else:
-    from . import tornadoserver as serve
-
 from .protocol import process_request
+from . import wrapper
+from . import serve
 
 __version__ = '0.0.2'
 
